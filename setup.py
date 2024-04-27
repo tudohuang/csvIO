414ead14a833c07f7b30f83c86375bce0e07b016
TudoHuang <88125758+tudohuang@users.noreply.github.com>
2024-04-27 08:40:45 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='csvIO',
+    version='0.1.0',
+    packages=find_packages(),
+    description='Simple authentication module for CSV based user management',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='TudoHuang',
+    author_email='tudohuang@gmail.com',
+    url='https://github.com/tudohuang/csvIO',
+    install_requires=[
+        'hashlib',  
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
